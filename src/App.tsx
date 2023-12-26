38e048bc6ea200d03593d5b870d29e8ca0345d4f
rodrigoecycle <rodrigo@ecycle.com.br>
2023-12-26 15:13:32 -0300

    :sparkles: feat: Configurações das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-
-      <h1>Hello World</h1>
     </ThemeProvider>
   )
 }
